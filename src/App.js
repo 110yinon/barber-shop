c8ae9f1c010065a4ff22545e069997f634451766
unknown <44569804+110yinon@users.noreply.github.com>
2020-04-17 14:15:25 +0300

    added comps to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './styles.css';
 import Header from './comps/Header'
+import Main from './comps/Main'
+import AlbumList from './comps/AlbumList';
+import Footer from './comps/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Main />
+      <AlbumList />
+      <Footer />
     </div>
   );
 }

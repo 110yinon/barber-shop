301ab6a681a1a6670dfcbdfd5f207ff05f0ba5ed
unknown <44569804+110yinon@users.noreply.github.com>
2020-04-20 14:14:43 +0300

    complete mobile responsive

--- src/comps/Header.js
+++ src/comps/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebookSquare,FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
 import { IoIosCall } from "react-icons/io";
 
 const Header = () => {
@@ -7,10 +7,15 @@ const Header = () => {
         <div className="header grid">
             <h3 className="full-name">Yossi Yakir</h3>
             <h1 className="barber-shop">Barber Shop</h1>
-            <p className="phone">‏053-673-4408‏</p>
-            <FaFacebookSquare className="facebook"/>
-            <FaInstagram className="instagram"/>
-            <IoIosCall className="call"/>
+            <div className="phone">
+                <IoIosCall className="call" />
+                <p>‏053-673-4408‏</p>
+            </div>
+            <div className="socials">
+                <FaFacebookSquare className="facebook" />
+                <FaInstagram className="instagram" />
+            </div>
+
         </div>
     );
 }

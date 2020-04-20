04c4599626470e53fa435ab304b35c4242dd635a
unknown <44569804+110yinon@users.noreply.github.com>
2020-04-20 20:38:24 +0300

    complete tablet responsive

--- src/comps/AlbumList.js
+++ src/comps/AlbumList.js
@@ -4,14 +4,14 @@ import kidsplayground from '../assets/kidsplayground.jpg'
 
 const AlbumList = () => {
     return (
-        <div className="album-list grid">
+        <div className="album-list tablet-gap grid">
             <img src={shop} alt="shop"/>
             <img src={kidsplayground} alt="kids plygrnd"/>
             <img src={require('../assets/hair7.jpg')} alt="hair7"/>
-            <img src={require('../assets/hair5.jpg')} alt="hair5"/>
+            <img src={require('../assets/hair5.jpg')} alt="hair5" className="propotion-pic"/>
             <img src={require('../assets/hair3.jpg')} alt="hair3"/>
-            <img src={require('../assets/hair4.jpg')} alt="hair4"/>
-            <img src={require('../assets/hair2.jpg')} alt="hair2"/>
+            <img src={require('../assets/hair4.jpg')} alt="hair4" className="propotion-pic"/>
+            <img src={require('../assets/hair2.jpg')} alt="hair2" className="propotion-pic"/>
             <img src={require('../assets/hair6.jpg')} alt="hair6"/>
         </div>
     );

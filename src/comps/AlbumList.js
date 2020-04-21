61bb76b2b0443c2481a2b90020a74a19d4c96609
unknown <44569804+110yinon@users.noreply.github.com>
2020-04-21 12:39:52 +0300

    added classes to each img

--- src/comps/AlbumList.js
+++ src/comps/AlbumList.js
@@ -4,15 +4,15 @@ import kidsplayground from '../assets/kidsplayground.jpg'
 
 const AlbumList = () => {
     return (
-        <div className="album-list tablet-gap grid">
-            <img src={shop} alt="shop"/>
-            <img src={kidsplayground} alt="kids plygrnd"/>
-            <img src={require('../assets/hair7.jpg')} alt="hair7"/>
-            <img src={require('../assets/hair5.jpg')} alt="hair5" className="propotion-pic"/>
-            <img src={require('../assets/hair3.jpg')} alt="hair3"/>
-            <img src={require('../assets/hair4.jpg')} alt="hair4" className="propotion-pic"/>
-            <img src={require('../assets/hair2.jpg')} alt="hair2" className="propotion-pic"/>
-            <img src={require('../assets/hair6.jpg')} alt="hair6"/>
+        <div className="album-list tablet-gap">
+            <img src={shop} alt="shop" className="pic1" />
+            <img src={kidsplayground} alt="kids plygrnd" className="pic2" />
+            <img src={require('../assets/hair7.jpg')} alt="hair7" className="pic3" />
+            <img src={require('../assets/hair5.jpg')} alt="hair5" className="pic4 propotion-pic" />
+            <img src={require('../assets/hair3.jpg')} alt="hair3" className="pic5" />
+            <img src={require('../assets/hair4.jpg')} alt="hair4" className="pic6 propotion-pic" />
+            <img src={require('../assets/hair2.jpg')} alt="hair2" className="pic7 propotion-pic" />
+            <img src={require('../assets/hair6.jpg')} alt="hair6" className="pic8" />
         </div>
     );
 }

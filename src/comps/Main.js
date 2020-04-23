020e2226f4acd06927c5722dfcc0a4901b127567
unknown <44569804+110yinon@users.noreply.github.com>
2020-04-23 13:42:45 +0300

    akjtkajwtklaj

--- src/comps/Main.js
+++ src/comps/Main.js
@@ -4,7 +4,7 @@ import About from './About';
 
 const Main = () => {
     return (
-        <div className="main tablet-gap grid">
+        <div className="main tablet-gap grid-rows grid">
             <About />
             <AlbumList />
         </div>
